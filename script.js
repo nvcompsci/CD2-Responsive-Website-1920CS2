5b47682db7bc557c2ca33df3bc958af328c33c11
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-08-30 13:56:47 -0400

    created script.js to create store data

---
+++ script.js
@@ -0,0 +1,19 @@
+//const cannot be changed
+// [] is an array {} is an object
+// this format is called JSON
+const items = [
+    {
+        "name":"t-shirt",
+        "price":12.99,
+        "supply":4,
+        "shippingCost":2.99,
+        "img":"tshirt.png"
+    },
+    {
+        "name":"coffee mug",
+        "price":4.99,
+        "supply":1,
+        "shippingCost":2.99,
+        "img":"coffeeMug.jpg"
+    },
+]
