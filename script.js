020602d1006745b604f06e53bb788018dc32a28f
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-09-03 14:04:52 -0400

    implement loadStore method

--- script.js
+++ script.js
@@ -12,8 +12,30 @@ const items = [
     {
         "name":"coffee mug",
         "price":4.99,
-        "supply":1,
+        "supply":0,
         "shippingCost":2.99,
         "img":"coffeeMug.jpg"
     },
 ]
+
+function loadStore() {
+    let container = document.createElement('div')
+    container.innerHTML = items.map( item => `<div class="card">
+<h3>${item.name}</h3>
+${item.supply <= 0 ? "<h2>SOLD OUT!</h2>" : ''}
+<p>$${item.price}</p>
+<img class="item" src=${item.img}>
+</div>` ).join('')
+    document.querySelector('article').append(container)
+    console.log(container.innerHTML)
+}
+
+
+
+
+
+
+
+
+
+
